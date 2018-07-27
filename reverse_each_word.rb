538a828f24f263f4a21dd90e2a475c273eb54d03
Engin Bozkurt <engin.bozkurt034@gmail.com>
2018-07-27 11:45:12 +0000

    differene between each and collect/map

---
+++ reverse_each_word.rb
@@ -0,0 +1,15 @@
+def reverse_each_word(sentence)
+   words = []
+   sentence.split(" ").each do |word|
+     words.push(word.reverse)
+   end
+   words.join(" ")
+end
+
+def reverse_each_word(sentence)
+   words = sentence.split(" ")
+   words.collect do |word|
+     word.reverse!
+   end
+   words.join(" ")
+end
